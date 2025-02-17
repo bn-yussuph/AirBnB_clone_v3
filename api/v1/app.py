e8e2f13c55c8f1bb942db61bd1d183433c6c9199
bn-yussuph <sulehajjaajj@gmail.com>
2025-02-17 08:36:59 +0000

    completed not found error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """app entry point"""
 
-from flask import Flask
-from models import storage
+from flask import Flask, jsonify, make_response
+#from models import storage
 from api.v1.views import app_views
 from os import getenv
+from models import storage
 
 app = Flask(__name__)
 
@@ -14,6 +15,14 @@ app.register_blueprint(app_views)
 def tear(self):
     """close storage engine"""
     storage.close()
+
+@app.errorhandler(404)
+def handle_404(exception):
+    code = exception.__str__().split()[0]
+    message = {"error": "Not found"}
+   # if code == 404:
+   #     message = {"error": "Not found"}
+    return make_response(jsonify(message), code)
 
 if __name__ == '__main__':
     if getenv("HBNB_API_HOST") is None:
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,25 @@
 """index file"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State", "cities": "City", "amenities": "Amenity", "reviews": "Review"}
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """routes to status page"""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """retrieve the number of each object by type"""
+    count = {}
+    for cls in classes:
+        count[cls] = storage.count(classes[cls])
+    return jsonify(count)
